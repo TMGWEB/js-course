114c7ddcbb318692f6eb72c4efc25996eee3035f
Tyler <test@email.com>
2018-12-28 12:30:44 -0500

    Learned about variables and data types.

---
+++ app.js
@@ -0,0 +1,115 @@
+/////////////////////////// Log to console
+/*
+console.log('Hello World');
+console.log('123');
+console.log(true);
+var greeting = "Hello";
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is some error');
+console.clear();
+console.warn('This is a warning');
+
+console.time('Hello');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.timeEnd('Hello');
+*/
+
+
+//////////////// VAR
+/* 
+var name = "John Doe";
+console.log(name);
+name = "Steve Smith";
+console.log(name);
+
+// init var
+var greeting;
+console.log(greeting);
+greeting = "Hello";
+console.log(greeting);
+
+// letters, numbers, _, $
+// Cannot start with a number
+
+// Multi word vars
+var firstName = "John"; // Camel case
+var first_name = "Sara"; // Underscore
+var FirstName = 'Tom'; // Pascal cacse
+var firstname;
+*/
+
+///////////////////LET
+/* 
+let name = "John Doe";
+console.log(name);
+name = "Steve Smith";
+console.log(name);
+*/
+
+///////////////// CONST
+/*
+const name = "John Doe";
+console.log(name);
+Cannot reassign
+name = "Steve Smith";
+Have to assign a value
+const greeting;
+
+const person = {
+  name: 'John',
+  age: 30
+}
+
+person.name = 'Sara';
+person.age = 32;
+
+// console.log(person);
+
+const numbers =[1,2,3,4,5];
+numbers.push(6);
+
+console.log(numbers);
+*/
+
+///////////////////// Data Types
+/*
+PRIMITIVE
+
+// Strings
+const name = 'John Doe';
+// Number
+const age = 45;
+// Boolean
+const hasKids = true;
+//  Null 
+const car = null;
+//  Undefined
+let test;
+//  Symbol
+const sym = Symbol();
+
+
+// REFERENCE
+// Array
+const hobbies = ['movies', 'music'];
+// Object literal
+const address = {
+  city: 'Orlando',
+  state: 'FL'
+}
+// Date
+const today = new Date();
+console.log(today);
+console.log( typeof today);
+*/
