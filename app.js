d442bd370e189e1877bcadaf13d1af616bcf4ab0
Tyler <test@email.com>
2019-01-05 17:25:46 -0500

    Learned about template literals.

--- app.js
+++ app.js
@@ -260,3 +260,39 @@ console.log( typeof today);
 // console.log(val);
 
 /////////////// SECTION 2 LECTURE 11 ///////////////
+// const name = 'John';
+// const age = 31;
+// const job = 'Web Developer';
+// const city = 'Miami';
+// let html;
+
+// Without template stringa (es5)
+// html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + '</li><li>Job: ' + job + '</li><li>City: ' + city + '</li></ul>';
+
+// html = '<ul>' + 
+//         '<li>Name: ' + name + '</li>' +
+//         '<li>Age: ' + age + '</li>' +
+//         '<li>Job: ' + job + '</li>' +
+//         '<li>City: ' + city + '</li>' +
+//         '</ul>';
+
+// function hello(){
+//   return 'hello';
+// }
+
+// With template literals/template strings (es6)
+// html = `
+//   <ul>
+//     <li>Name: ${name}</li>
+//     <li>Age: ${age}</li>
+//     <li>Job: ${job}</li>
+//     <li>City: ${city}</li>
+//     <li>${2 + 2}</li>
+//     <li>${hello()}</li>
+//     <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+//   </ul>
+// `;
+
+// document.body.innerHTML = html;
+
+/////////////// SECTION 2 LECTURE 12 ///////////////
